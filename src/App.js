59f5b26a6fa684bfcabba188f05f6e0305dfac52
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-05-19 15:32:18 +0600

    react lerning start-first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit name is jawad <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
